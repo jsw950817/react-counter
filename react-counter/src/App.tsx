1474a5dbe7ea4af13342bb70fcd4bf4e48164e31
jeon Seungwon <wjstmddnjs5@daum.net>
2024-05-29 12:58:40 +0900

    증가버튼 구현

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -1,12 +1,16 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const 리턴값 = useState(0);
+  const count = 리턴값[0];
+  const reCount = 리턴값[1];
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={()=>{console.log("클릭됨")}}>증가</button>
+      <h1>{count}</h1>
+      <button onClick={()=> reCount((현재count)=> 현재count+ 1)}>증가</button>
     </div>
   );
 }
