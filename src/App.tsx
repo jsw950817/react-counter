62cb87b84e88527f42c0bc4b0034399e4511020a
jeon Seungwon <wjstmddnjs5@daum.net>
2024-05-29 15:58:28 +0900

    0일때 초기화 숨기기

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ function App() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
   const reCount = 리턴값[1];
+
+  let 초기화버튼; 
+  if (count !== 0) {
+    초기화버튼 = <button onClick={() => reCount(0)}>초기화</button>; 
+  }
+
   return (
     <div>
       <h1>{count}</h1>
@@ -16,7 +22,7 @@ function App() {
       <button onClick={() => reCount((현재count) => 현재count - 1)}>
         감소
       </button>{" "}
-      <button onClick={() => reCount(0)}>초기화</button>
+      {초기화버튼}
     </div>
   );
 }
