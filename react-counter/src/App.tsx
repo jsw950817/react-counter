39fcfb5c6c658644cb2241de589d721319fc49b0
jeon Seungwon <wjstmddnjs5@daum.net>
2024-05-29 12:38:23 +0900

    기본적인 틀

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -2,9 +2,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>0</h1>
+      <button>증가</button>
+    </div>
+  );
 }
 
 export default App;
